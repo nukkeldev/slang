2becfcedb713a175fa5e5c7700dd138a3520edfa
nukkeldev <110308379+nukkeldev@users.noreply.github.com>
2025-05-26 00:31:17 -0700

    added slang-capability-generator and slang-embed tools and platform-specific sources

--- build.zig
+++ build.zig
@@ -1,10 +1,102 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
+var target: std.Build.ResolvedTarget = undefined;
+var optimize: std.builtin.OptimizeMode = undefined;
 var upstream: *std.Build.Dependency = undefined;
 
+// Build
+
+pub fn build(b: *std.Build) !void {
+    // Upstream
+    upstream = b.dependency("slang", .{});
+
+    // Options
+    target = b.standardTargetOptions(.{});
+    optimize = b.standardOptimizeOption(.{});
+
+    const linkage = b.option(std.builtin.LinkMode, "linkage", "The link mode for the library.") orelse .static;
+    // TODO: Define the rest of the options.
+
+    // Libraries
+    const lib_core = try addTarget(b, "core", .{
+        .type = .library,
+        .path = "source/core",
+        .external_dependencies = &.{ .miniz, .unordered_dense, .lz4 },
+        .include_directories = &.{ "source", "include" },
+        .linkage = linkage,
+    });
+    b.installArtifact(lib_core);
+
+    const lib_compiler_core = try addTarget(b, "compiler-core", .{
+        .type = .library,
+        .path = "source/compiler-core",
+        .link_with = &.{lib_core}, // TODO: Propigate
+        .external_dependencies = &.{ .@"spirv-headers", .unordered_dense },
+        .include_directories = &.{ "source", "include" },
+        .file_specific_flags = &.{.{ .file = "slang-dxc-compiler.cpp", .flags = &.{"-fms-extensions"} }},
+        .linkage = linkage,
+    });
+    b.installArtifact(lib_compiler_core);
+
+    // const prelude = try addTarget(b, mod, "prelude", .{
+    //     .type = .library,
+    //     .path = "prelude",
+    //     .include_directories = &.{ "source", "include" },
+    // });
+
+    // Tools
+    // -- slang-capability-generator
+    const exe_capability_generator = try addTarget(b, "slang-capability-generator", .{
+        .type = .executable,
+        .path = "tools/slang-capability-generator",
+        .include_directories = &.{"include"},
+        .link_with = &.{lib_compiler_core},
+        .external_dependencies = &.{.unordered_dense},
+        .linkage = .dynamic,
+    });
+    b.installArtifact(exe_capability_generator);
+
+    const run_capability_generator = b.addRunArtifact(exe_capability_generator);
+    run_capability_generator.cwd = upstream.path("");
+    run_capability_generator.addArgs(&.{
+        "source/slang/slang-capabilities.capdef",
+        "--target-directory",
+        "source/slang/capability",
+        "--doc",
+        "docs/user-guide/a3-02-reference-capability-atoms.md",
+    });
+    // -- slang-embed
+    const exe_embed = try addTarget(b, "slang-embed", .{
+        .type = .executable,
+        .path = "tools/slang-embed",
+        .include_directories = &.{"include"},
+        .link_with = &.{lib_core},
+        .external_dependencies = &.{.unordered_dense},
+        .linkage = .dynamic,
+    });
+    b.installArtifact(exe_embed);
+
+    const run_embed = b.addRunArtifact(exe_capability_generator);
+    run_embed.cwd = upstream.path("");
+    run_embed.addArgs(&.{
+        "source/slang/slang-capabilities.capdef",
+        "--target-directory",
+        "source/slang/capability",
+        "--doc",
+        "docs/user-guide/a3-02-reference-capability-atoms.md",
+    });
+}
+
+// Targets
+
 const TargetOptions = struct {
+    type: enum { library, executable },
+
     path: []const u8,
-    include_directories: []const []const u8,
+    link_with: []const *std.Build.Step.Compile = &.{},
+    external_dependencies: []const enum { lz4, miniz, @"spirv-headers", unordered_dense } = &.{},
+    include_directories: []const []const u8 = &.{},
 
     file_specific_flags: []const struct {
         file: []const u8,
@@ -14,15 +106,19 @@ const TargetOptions = struct {
     linkage: std.builtin.LinkMode = .static,
 };
 
-fn addTarget(b: *std.Build, mod: *std.Build.Module, name: []const u8, options: TargetOptions) !*std.Build.Step.Compile {
+fn addTarget(b: *std.Build, name: []const u8, options: TargetOptions) !*std.Build.Step.Compile {
     const root = upstream.path(options.path);
 
-    // Library
-    const lib = b.addLibrary(.{
-        .name = name,
-        .root_module = mod,
-        .linkage = options.linkage,
+    // Module
+    const mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libcpp = true,
+        .pic = true,
     });
+
+    // Link With
+    for (options.link_with) |link_with| mod.linkLibrary(link_with);
 
     // Add C++ Sources
     {
@@ -32,104 +128,100 @@ fn addTarget(b: *std.Build, mod: *std.Build.Module, name: []const u8, options: T
         }
         defer file_specific_flags.deinit();
 
+        const allowed_dirs: []const []const u8 = &[_][]const u8{""} ++ switch (builtin.os.tag) {
+            .windows => &[_][]const u8{"windows"},
+            .linux => &[_][]const u8{"unix"},
+            else => &[_][]const u8{},
+        };
+
         const dir = try upstream.path(options.path).getPath3(b, null).openDir(".", .{ .iterate = true });
-        var iter = dir.iterate();
+        var iter = try dir.walk(b.allocator);
+        defer iter.deinit();
 
         while (try iter.next()) |entry| {
-            if (entry.kind != .file or !std.mem.endsWith(u8, entry.name, ".cpp")) continue;
-            lib.addCSourceFile(.{
-                .file = root.path(b, entry.name),
-                .flags = file_specific_flags.get(entry.name) orelse &.{},
+            if (entry.kind != .file or !std.mem.endsWith(u8, entry.basename, ".cpp")) continue;
+
+            if (std.fs.path.dirname(entry.path)) |dirname| {
+                var allowed = false;
+                for (allowed_dirs) |allowed_dir| if (std.mem.eql(u8, allowed_dir, dirname)) {
+                    allowed = true;
+                    break;
+                };
+                if (!allowed) continue;
+            }
+
+            mod.addCSourceFile(.{
+                .file = root.path(b, entry.path),
+                .flags = file_specific_flags.get(entry.path) orelse &.{},
             });
         }
-
-        // TODO: Add platform-specific dependencies for i.e. `windows/` sub-folder.
     }
 
     // Include Directories
-    lib.addIncludePath(root);
-    for (options.include_directories) |dir| lib.addIncludePath(upstream.path(dir));
-
-    // TODO: installHeadersDirectory?
-
-    return lib;
-}
-
-pub fn build(b: *std.Build) !void {
-    // Upstream
-    upstream = b.dependency("slang", .{});
-
-    // Options
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    const linkage = b.option(std.builtin.LinkMode, "linkage", "The link mode for the library.") orelse .static;
-    // TODO: Define the rest of the options.
-
-    // External Dependencies
-    // -- Raw
-    const miniz = b.dependency("miniz", .{});
-    const unordered_dense = b.dependency("unordered_dense", .{});
-    const spriv_headers = b.dependency("spirv-headers", .{});
-
-    // -- Wrapped
-    const lz4 = b.dependency("lz4", .{
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // Module
-    const mod_slang = b.addModule("slang", .{
-        .target = target,
-        .optimize = optimize,
-        .link_libc = true,
-        .link_libcpp = true,
-        .pic = true,
-        // .sanitize_c = null, // ?
-    });
-
-    // External Dependency Installation
-    // -- miniz
-    mod_slang.addIncludePath(miniz.path(""));
-    mod_slang.addCSourceFile(.{ .file = miniz.path("miniz.c") });
-    // -- ankerl/unordered_dense
-    mod_slang.addIncludePath(unordered_dense.path("include"));
-    // -- spirv-headers
-    mod_slang.addIncludePath(spriv_headers.path("include"));
-    // -- lz4
-    mod_slang.linkLibrary(lz4.artifact("lz4"));
+    mod.addIncludePath(root);
+    for (options.include_directories) |dir| mod.addIncludePath(upstream.path(dir));
 
     // Compiler Definitions
-    switch (linkage) {
+    switch (options.linkage) {
         .dynamic => {
-            mod_slang.addCMacro("SLANG_DYNAMIC", "");
-            mod_slang.addCMacro("SLANG_DYNAMIC_EXPORT", "");
+            mod.addCMacro("SLANG_DYNAMIC", "");
+            mod.addCMacro("SLANG_DYNAMIC_EXPORT", "");
         },
         .static => {
-            mod_slang.addCMacro("SLANG_STATIC", "");
+            mod.addCMacro("SLANG_STATIC", "");
         },
     }
 
-    // Libraries
-    // const lib = b.addLibrary(.{
-    //     .name = "slang",
-    //     .root_module = mod_slang,
-    //     .linkage = linkage,
-    // });
-    // b.installArtifact(lib);
+    // External Dependencies
+    for (options.external_dependencies) |dep| switch (dep) {
+        .miniz => miniz(b, mod),
+        .unordered_dense => unordered_dense(b, mod),
+        .@"spirv-headers" => spriv_headers(b, mod),
+        .lz4 => lz4(b, mod),
+    };
 
-    const lib_core = try addTarget(b, mod_slang, "core", .{
-        .path = "source/core",
-        .include_directories = &.{ "source", "include" },
-        .linkage = linkage,
-    });
-    b.installArtifact(lib_core);
+    // TODO: installHeadersDirectory?
 
-    const lib_compiler_core = try addTarget(b, mod_slang, "compiler-core", .{
-        .path = "source/compiler-core",
-        .include_directories = &.{ "source", "include" },
-        .file_specific_flags = &.{.{ .file = "slang-dxc-compiler.cpp", .flags = &.{"-fms-extensions"} }},
-        .linkage = linkage,
+    return switch (options.type) {
+        .library => b.addLibrary(.{
+            .name = name,
+            .root_module = mod,
+            .linkage = options.linkage,
+        }),
+        .executable => b.addExecutable(.{
+            .name = name,
+            .root_module = mod,
+            .linkage = options.linkage,
+        }),
+    };
+}
+
+// External Dependencies
+
+fn miniz(b: *std.Build, mod: *std.Build.Module) void {
+    const dep = b.dependency("miniz", .{});
+
+    mod.addIncludePath(dep.path(""));
+    mod.addCSourceFile(.{ .file = dep.path("miniz.c") });
+}
+
+fn unordered_dense(b: *std.Build, mod: *std.Build.Module) void {
+    const dep = b.dependency("unordered_dense", .{});
+
+    mod.addIncludePath(dep.path("include"));
+}
+
+fn spriv_headers(b: *std.Build, mod: *std.Build.Module) void {
+    const dep = b.dependency("spirv-headers", .{});
+
+    mod.addIncludePath(dep.path("include"));
+}
+
+fn lz4(b: *std.Build, mod: *std.Build.Module) void {
+    const dep = b.dependency("lz4", .{
+        .target = target,
+        .optimize = optimize,
     });
-    b.installArtifact(lib_compiler_core);
+
+    mod.linkLibrary(dep.artifact("lz4"));
 }
