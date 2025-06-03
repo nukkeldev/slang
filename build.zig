78eeb3e0cf71ddd1406e719d7096af7b698e72d5
nukkeldev <110308379+nukkeldev@users.noreply.github.com>
2025-06-02 22:31:30 -0700

    added extra steps and some of the dependencies

--- build.zig
+++ build.zig
@@ -1,5 +1,17 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b;
+    // Get the upstream's file tree.
+    const upstream = b.dependency("upstream", .{}).path(".");
+    if (b.verbose) std.log.info("Upstream Path: {}", .{upstream.dependency.dependency.builder.build_root});
+
+    // -- Extra Steps --
+
+    // Create an unpack step to view the source code we are using.
+    const unpack = b.step("unpack", "Installs the unpacked source");
+    unpack.dependOn(&b.addInstallDirectory(.{
+        .source_dir = upstream,
+        .install_dir = .{ .custom = "unpacked" },
+        .install_subdir = "",
+    }).step);
 }
